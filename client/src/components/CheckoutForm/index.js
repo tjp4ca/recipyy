d004a4bebd4209fcd1535a92c40e9d0377d848ab
kea6t <keasay26@gmail.com>
2022-10-26 15:40:57 -0400

    new stripe form

--- client/src/components/CheckoutForm/index.js
+++ client/src/components/CheckoutForm/index.js
@@ -1,88 +1,136 @@
-import React from "react";
-import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
-import axios from "axios";
+// import React from "react";
+// import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
+// import axios from "axios";
 
 
+// export const CheckoutForm = () => {
+//   const stripe = useStripe();
+//   const elements = useElements();
 
-// const appearance = {
-//   theme: 'stripe',
+//   const handleSubmit = async (event) => {
+//     event.preventDefault();
+//     const { error, paymentMethod } = await stripe.createPaymentMethod({
+//       type: "card",
+//       card: elements.getElement(CardElement),
+//     });
 
-//   variables: {
-//     colorPrimary: '#0570de',
-//     colorBackground: '#ffffff',
-//     colorText: '#30313d',
-//     colorDanger: '#df1b41',
-//     fontFamily: 'Ideal Sans, system-ui, sans-serif',
-//     spacingUnit: '2px',
-//     borderRadius: '4px',
-//     // See all possible variables below
-//   }
+//     if (!error) {
+//       console.log("Stripe 23 | token generated!", paymentMethod);
+//       try {
+//         const { id } = paymentMethod;
+//         const response = await axios.post(
+//           "http://localhost:3001/stripe/charge",
+//           {
+//             amount: 2000,
+//             id: id,
+//           }
+//         );
+
+//         console.log("Stripe 35 | data", response.data.success);
+//         if (response.data.success) {
+//           console.log("CheckoutForm.js 25 | payment successful!");
+//         }
+//       } catch (error) {
+//         console.log("CheckoutForm.js 28 | ", error);
+//       }
+//     } else {
+//       console.log(error.message);
+//     }
+//   };
+
+
+//   return (
+//     <div className="row justify-content-center">
+//       <form className="donateForm col"
+//         onSubmit={handleSubmit} style={{ maxWidth: 400 }}>
+
+//         <CardElement className="cardInfo col-12" />
+
+//         <button className="payBtn col-12">Pay</button>
+
+//       </form>
+//     </div>
+
+//   );
 // };
 
-// // Pass the appearance object to the Elements instance
-// const elements = stripe.elements({clientSecret, appearance});
 
+import React, { useEffect, useState } from "react";
+import {
+  PaymentElement,
+  useStripe,
+  useElements
+} from "@stripe/react-stripe-js";
 
-
-
-
-
-
-
-
-
-
-
-export const CheckoutForm = () => {
+export default function CheckoutForm(){
   const stripe = useStripe();
   const elements = useElements();
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    const { error, paymentMethod } = await stripe.createPaymentMethod({
-      type: "card",
-      card: elements.getElement(CardElement),
-    });
-
-    if (!error) {
-      console.log("Stripe 23 | token generated!", paymentMethod);
-      try {
-        const { id } = paymentMethod;
-        const response = await axios.post(
-          "http://localhost:3001/stripe/charge",
-          {
-            amount: 2000,
-            id: id,
-          }
-        );
-
-        console.log("Stripe 35 | data", response.data.success);
-        if (response.data.success) {
-          console.log("CheckoutForm.js 25 | payment successful!");
-        }
-      } catch (error) {
-        console.log("CheckoutForm.js 28 | ", error);
-      }
-    } else {
-      console.log(error.message);
+  const [message, setMessage] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  useEffect(() => {
+    if (!stripe) {
+      return;
     }
+    const clientSecret = new URLSearchParams(window.location.search).get(
+      "payment_intent_client_secret"
+    );
+    if (!clientSecret) {
+      return;
+    }
+    stripe.retrievePaymentIntent(clientSecret).then(({ paymentIntent }) => {
+      switch (paymentIntent.status) {
+        case "succeeded":
+          setMessage("Payment succeeded!");
+          break;
+        case "processing":
+          setMessage("Your payment is processing.");
+          break;
+        case "requires_payment_method":
+          setMessage("Your payment was not successful, please try again.");
+          break;
+        default:
+          setMessage("Something went wrong.");
+          break;
+      }
+    });
+  }, [stripe]);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!stripe || !elements) {
+      // Stripe.js has not yet loaded.
+      // Make sure to disable form submission until Stripe.js has loaded.
+      return;
+    }
+    setIsLoading(true);
+    const { error } = await stripe.confirmPayment({
+      elements,
+      confirmParams: {
+        // Make sure to change this to your payment completion page
+        return_url: "http://localhost:3000",
+      },
+    });
+    // This point will only be reached if there is an immediate error when
+    // confirming the payment. Otherwise, your customer will be redirected to
+    // your `return_url`. For some payment methods like iDEAL, your customer will
+    // be redirected to an intermediate site first to authorize the payment, then
+    // redirected to the `return_url`.
+    if (error.type === "card_error" || error.type === "validation_error") {
+      setMessage(error.message);
+    } else {
+      setMessage("An unexpected error occurred.");
+    }
+    setIsLoading(false);
   };
-
-
-
-
-
   return (
-    <div className="row justify-content-center">
-      <form className="donateForm col"
-        onSubmit={handleSubmit} style={{ maxWidth: 400 }}>
-
-        <CardElement className="cardInfo col-12" />
-
-        <button className="payBtn col-12">Pay</button>
-
-      </form>
-    </div>
-
+    <form id="payment-form" onSubmit={handleSubmit}>
+      <PaymentElement id="payment-element" />
+      <button disabled={isLoading || !stripe || !elements} id="submit">
+        <span id="button-text">
+          {isLoading ? <div className="spinner" id="spinner"></div> : "Pay now"}
+        </span>
+      </button>
+      {/* Show any error or success messages */}
+      {message && <div id="payment-message">{message}</div>}
+    </form>
   );
-};
+}
--- server/server.js
+++ server/server.js
@@ -18,12 +18,12 @@ const server = new ApolloServer({
 
 const app = express();
 
-// app.use(express.urlencoded({ extended: false }));
-// app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(cors());
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+// app.use(cors());
 
 // Serve up static assets
 if (process.env.NODE_ENV === 'production') {
@@ -34,31 +34,57 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-app.post("/stripe/charge", cors(), async (req, res) => {
-  console.log("stripe-routes.js 9 | route reached", req.body);
-  let { amount, id } = req.body;
-  console.log("stripe-routes.js 10 | amount and id", amount, id);
-  try {
-    const payment = await stripe.paymentIntents.create({
-      amount: amount,
-      currency: "USD",
-      description: "Your Company Description",
-      payment_method: id,
-      confirm: true,
-    });
-    console.log("stripe-routes.js 19 | payment", payment);
-    res.json({
-      message: "Payment Successful",
-      success: true,
-    });
-  } catch (error) {
-    console.log("stripe-routes.js 17 | error", error);
-    res.json({
-      message: "Payment Failed",
-      success: false,
-    });
-  }
+
+// app.use(express.static("public"));
+// app.use(express.json());
+const calculateOrderAmount = (items) => {
+  // Replace this constant with a calculation of the order's amount
+  // Calculate the order total on the server to prevent
+  // people from directly manipulating the amount on the client
+  return 1400;
+};
+app.post("/create-payment-intent", async (req, res) => {
+  const { items } = req.body;
+  // Create a PaymentIntent with the order amount and currency
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: calculateOrderAmount(items),
+    currency: "usd",
+    automatic_payment_methods: {
+      enabled: true,
+    },
+  });
+  res.send({
+    clientSecret: paymentIntent.client_secret,
+  });
 });
+
+
+
+// app.post("/stripe/charge", cors(), async (req, res) => {
+//   console.log("stripe-routes.js 9 | route reached", req.body);
+//   let { amount, id } = req.body;
+//   console.log("stripe-routes.js 10 | amount and id", amount, id);
+//   try {
+//     const payment = await stripe.paymentIntents.create({
+//       amount: amount,
+//       currency: "USD",
+//       description: "Your Company Description",
+//       payment_method: id,
+//       confirm: true,
+//     });
+//     console.log("stripe-routes.js 19 | payment", payment);
+//     res.json({
+//       message: "Payment Successful",
+//       success: true,
+//     });
+//   } catch (error) {
+//     console.log("stripe-routes.js 17 | error", error);
+//     res.json({
+//       message: "Payment Failed",
+//       success: false,
+//     });
+//   }
+// });
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
