93fcbf73fcaeb175420e71b9d4b19d0e47bc17ad
Matt Neal <neal.matt.m@gmail.com>
2022-10-25 20:36:23 -0400

    minor change to connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/recipr',
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/recipyy',
   {
     useNewUrlParser: true, 
     useUnifiedTopology: true,
